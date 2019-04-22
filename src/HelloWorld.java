830a91399d9b3474cba4eab88094b20ed13a13af
Kaushal Kumar <sirkaushalkumar@gmail.com>
2019-04-22 08:50:27 -0700

    Fix the sign

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld {
     static void checkAge(int age) {
 	if (age < 18) {
 	    throw new ArithmeticException(
-		    "Access denied - You must be at least 18 years old.");
+		    "Access denied - You must be at least 18 years old!!");
 	} else {
 	    System.out.println(
 		    "Access granted - You are old enough!");
